58a22b4a4cf0a3f08d8f66af0803674d10f19ad2
wbham <wbham97@naver.com>
2024-01-22 19:20:17 +0900

    [Feat] - 갤러리에서 사진 가져와서 업로드
    - 다크테마에 대비하여 primary, secondary color 지정
    - pickmedia를 통해 사진을 불러오면 imageview에 이미지 set
    - submit 버튼 클릭 시 Firebase에 업로드. 업로드가 완료되면 해당 파일의 url을 다운로드

--- app/src/main/java/org/shop/tomorrowhouse/ui/article/WriteArticleFragment.kt
+++ app/src/main/java/org/shop/tomorrowhouse/ui/article/WriteArticleFragment.kt
@@ -1,5 +1,6 @@
 package org.shop.tomorrowhouse.ui.article
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,20 +9,29 @@ import android.view.ViewGroup
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.Firebase
+import com.google.firebase.storage.storage
 import org.shop.tomorrowhouse.databinding.FragmentWriteArticleBinding
+import java.util.UUID
 
 class WriteArticleFragment : Fragment() {
     private lateinit var binding: FragmentWriteArticleBinding
 
-    val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-        // Callback is invoked after the user selects a media item or closes the
-        // photo picker.
-        if (uri != null) {
-            Log.d("PhotoPicker", "Selected URI: $uri")
-        } else {
-            Log.d("PhotoPicker", "No media selected")
+    private var selectedUri: Uri? = null
+    private val pickMedia =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            // Callback is invoked after the user selects a media item or closes the
+            // photo picker.
+            if (uri != null) {
+                Log.d("PhotoPicker", "Selected URI: $uri")
+                selectedUri = uri
+                binding.photoImageView.setImageURI(uri)
+            } else {
+                Log.d("PhotoPicker", "No media selected")
+            }
         }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,5 +46,42 @@ class WriteArticleFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+
+        binding.photoImageView.setOnClickListener {
+
+        }
+
+        binding.deleteButton.setOnClickListener {
+
+        }
+
+        binding.submitButton.setOnClickListener {
+            if (selectedUri != null) {
+                val photoUri = selectedUri ?: return@setOnClickListener
+                val fileName = "${UUID.randomUUID()}.png"
+                Firebase.storage.reference.child("articles/photo").child(fileName)
+                    .putFile(photoUri).addOnCompleteListener{ task ->
+                        if (task.isSuccessful){
+                            // 이미지가 성공적으로 업로드 됐으니까 다운로드 URL을 받아올 수 있다
+                            Log.e("WriteArticleFrag - UpLoad Success", task.toString())
+                            Firebase.storage.reference.child("articles/photo/$fileName").downloadUrl.addOnSuccessListener {
+                                Log.e("WriteArticleFrag - DownLoad Success", it.toString())
+
+                            }.addOnFailureListener {
+
+                            }
+                        }else{
+                            // Error Handler
+                            task.exception?.printStackTrace()
+                        }
+                    }
+            } else {
+                Snackbar.make(binding.root, "이미지가 선택되지 않았습니다.", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.backButton.setOnClickListener {
+            findNavController().navigate(WriteArticleFragmentDirections.actionBack())
+        }
     }
 }
